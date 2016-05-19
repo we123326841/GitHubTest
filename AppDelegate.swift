0e4519eb188bf7bf2b4a73e7248a922e486277f2
we123326841 <3150776785@qq.com>
2016-05-19 14:38:04 +0800

    Revert "Update AppDelegate.swift"
    This reverts commit ca5f5957d03922b3583c72a451696cfd8f7de806.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch
-        //日你妈
+        // Override point for customization after application launch.
         return true
     }
 
